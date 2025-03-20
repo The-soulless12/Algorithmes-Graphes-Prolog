72e5f3d10ba565b09012f864f3554365f5288801
The-soulless12 <aklirayane2@gmail.com>
2025-03-20 10:19:07 +0100

    Algo de Prim

--- main.pl
+++ main.pl
@@ -69,7 +69,7 @@ tri_decroissant_DS(Sommets, Affectations, SommetsTries) :-
     sort(0, @>=, Paires, Triees),
     findall(S, member(_-_ - S, Triees), SommetsTries).
 
-assigner_couleur_DS([], _, []).  
+assigner_couleur_DS([], _, []).  % Si la liste des sommets est vide, la coloration l est aussi
 assigner_couleur_DS(Sommets, Affectations, [(S-C)|Autres]) :-
     tri_decroissant_DS(Sommets, Affectations, [S|_]), 
     between(1, 100, C),
@@ -83,6 +83,21 @@ d_satur(Coloration) :-
 
 % Les algorithmes de recherche d arbres couvrants minimaux
 %Algo03 : Prim
+prim_recursif(SommetsVisites, Arbres, Arbres) :- % Si tous les sommets sont couverts
+    tous_les_sommets(Tous),
+    subset(Tous, SommetsVisites). % On vérifie si tous les sommets ont déjà été visités
+
+prim_recursif(SommetsVisites, ArbresActuels, Arbre) :- % Sinon il faut ajouter l arête de poids minimal
+    % Retourne toutes les arêtes reliant un sommet déjà visité à un sommet non visité
+    findall(Poids-U-V,
+            (member(U, SommetsVisites), arc(U, V, Poids), \+ member(V, SommetsVisites)),
+            ArcsPossibles),
+    sort(ArcsPossibles, [_-Umin-Vmin|_]),
+    prim_recursif([Vmin|SommetsVisites], [arc(Umin, Vmin, _)|ArbresActuels], Arbre).
+
+prim(Arbre) :-
+    tous_les_sommets([Depart|_]),
+    prim_recursif([Depart], [], Arbre).
 
 %Algo04 : Kruskal
 
